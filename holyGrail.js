4440266512d2398b7b214797204ba7aba66c42f9
Nishant Sharma <snishant842@gmail.com>
2026-09-15 17:31:46 +0530

    Create holyGrail.js

---
+++ holyGrail.js
@@ -0,0 +1,57 @@
+<div class="parent">
+<header>I am Header</header>
+<div class="left-nav">I am left Nav</div>
+<main>
+    I am Main Content
+</main>
+<div class="right-nav">I am Right Nav</div>
+<footer>I am Footer</footer>
+</div>
+
+
+// .parent{
+//     display: grid;
+//     grid-template: auto 1fr auto / 100px 1fr 100px;
+//     height:100vh;
+// }
+// header{
+//     background-color: yellow;
+//     grid-column: 1/4;
+// }
+// .left-nav{
+//    background-color: green;
+//    grid-column: 1/2;
+// }
+// .right-nav{
+//      background-color: pink;
+//       grid-column: 3/4;
+// }
+// footer{
+//      background-color: blue;
+//       grid-column: 1/4;
+// }
+// main{
+//      background-color: red;
+//      grid-column: 2/3;
+// }
+// header,
+//     .left-nav,
+//     .right-nav,
+//     main,
+//     footer{
+//         padding:10px;
+//     }
+
+// @media(max-width:768px){
+//     .parent{
+//         grid-template: repeat(5,1fr) / auto;
+//     }
+    
+//     header,
+//     .left-nav,
+//     .right-nav,
+//     main,
+//     footer{
+//         grid-column: 1/2;
+//     }
+// }
